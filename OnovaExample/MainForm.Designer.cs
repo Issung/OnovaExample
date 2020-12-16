7a1baf19128fd61bc3efde929752a906ca7bddaa
Joel G <joel@sonofgoran.com>
2020-12-17 00:33:59 +1100

    Initial commit. All working.

---
+++ OnovaExample/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace OnovaExample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.currentVersionLabel = new System.Windows.Forms.Label();
+            this.versionTextBox = new System.Windows.Forms.TextBox();
+            this.checkForUpdatesButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // currentVersionLabel
+            // 
+            this.currentVersionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.currentVersionLabel.AutoSize = true;
+            this.currentVersionLabel.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.currentVersionLabel.Location = new System.Drawing.Point(12, 9);
+            this.currentVersionLabel.Name = "currentVersionLabel";
+            this.currentVersionLabel.Size = new System.Drawing.Size(249, 25);
+            this.currentVersionLabel.TabIndex = 0;
+            this.currentVersionLabel.Text = "Current Application Version:";
+            // 
+            // versionTextBox
+            // 
+            this.versionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.versionTextBox.Enabled = false;
+            this.versionTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.versionTextBox.Location = new System.Drawing.Point(12, 42);
+            this.versionTextBox.Name = "versionTextBox";
+            this.versionTextBox.Size = new System.Drawing.Size(375, 29);
+            this.versionTextBox.TabIndex = 1;
+            this.versionTextBox.Text = "0.0.0";
+            // 
+            // checkForUpdatesButton
+            // 
+            this.checkForUpdatesButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkForUpdatesButton.Location = new System.Drawing.Point(12, 88);
+            this.checkForUpdatesButton.Name = "checkForUpdatesButton";
+            this.checkForUpdatesButton.Size = new System.Drawing.Size(375, 29);
+            this.checkForUpdatesButton.TabIndex = 2;
+            this.checkForUpdatesButton.Text = "Check for Updates";
+            this.checkForUpdatesButton.UseVisualStyleBackColor = true;
+            this.checkForUpdatesButton.Click += new System.EventHandler(this.checkForUpdatesButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Location = new System.Drawing.Point(12, 123);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(375, 29);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 162);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkForUpdatesButton);
+            this.Controls.Add(this.versionTextBox);
+            this.Controls.Add(this.currentVersionLabel);
+            this.MinimumSize = new System.Drawing.Size(304, 201);
+            this.Name = "MainForm";
+            this.Text = "Onova Updater Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button checkForUpdatesButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label currentVersionLabel;
+        private System.Windows.Forms.TextBox versionTextBox;
+    }
+}
+
---
+++ OnovaExample/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using Onova;
+using Onova.Services;
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace OnovaExample
+{
+    public partial class MainForm : Form
+    {
+        private readonly IUpdateManager updateManager = new UpdateManager(
+                new GithubPackageResolver("Issung", "OnovaExample", "OnovaExample-*.zip"),
+                new ZipPackageExtractor()
+            );
+
+        string currentVersion;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            currentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            versionTextBox.Text = currentVersion;
+        }
+
+        private async void checkForUpdatesButton_Click(object sender, EventArgs e)
+        {
+            // Check for updates
+            var check = await updateManager.CheckForUpdatesAsync();
+
+            // If there are none, notify user and return
+            if (!check.CanUpdate)
+            {
+                MessageBox.Show("There are no updates available.");
+                return;
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show(
+                    $"There is an update available. Your current version is {currentVersion}, the latest version is {check.LastVersion}. Would you like to update to the latest version?",
+                    "Update Available",
+                    MessageBoxButtons.YesNo
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    // Prepare the latest update
+                    await updateManager.PrepareUpdateAsync(check.LastVersion);
+
+                    // Launch updater and exit
+                    updateManager.LaunchUpdater(check.LastVersion);
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
